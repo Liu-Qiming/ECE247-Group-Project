f71676a1dda5fc16f0d6d821e8ed04d04903f6d8
Liu-Qiming <qimingliu0831@g.ucla.edu>
2024-03-03 21:32:32 -0800

    add CNN_LSTM model with 54% acc, add early stopping to training

--- src/methods.py
+++ src/methods.py
@@ -2,27 +2,46 @@ import torch
 from torch import nn
 from tqdm import tqdm
 
-def train(model, train_loader, valid_loader, criterion, optimizer, device, print_every=10, epochs=20):
+def train(model, train_loader, valid_loader, criterion, optimizer, device, print_every=10, epochs=20, patience=20):
+    cur_val_max = float('inf')
+    tol = 0
+
     for epoch in tqdm(range(epochs)):
         model.train()
         running_loss = 0.0
-
         for data in train_loader:
-            X, y = data
-            X, y = X.float().to(device), y.long().to(device)  
-
+            X, y = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(X)
             loss = criterion(outputs, y)
             loss.backward()
             optimizer.step()
-
             running_loss += loss.item() * X.size(0)
+
+        val_loss = 0.0
+        model.eval()
+        with torch.no_grad():
+            for data in valid_loader:
+                X, y = data[0].to(device), data[1].to(device)
+                outputs = model(X)
+                loss = criterion(outputs, y)
+                val_loss += loss.item() * X.size(0)
+
+        cur_val_mean_loss = val_loss / len(valid_loader.dataset)
 
         if (epoch + 1) % print_every == 0:
             avg_loss = running_loss / len(train_loader.dataset)
             val_accuracy = evaluate(model, valid_loader, device)
             print(f'Epoch [{epoch + 1}/{epochs}], Loss: {avg_loss:.4f}, Validation Accuracy: {val_accuracy:.2f}%')
+
+        if cur_val_mean_loss < cur_val_max:
+            cur_val_max = cur_val_mean_loss
+            tol = 0
+        else:
+            tol += 1
+            if tol == patience:
+                print('Early stopping!')
+                break
 
 def evaluate(model, test_loader, device):
     model.eval()
--- src/models.py
+++ src/models.py
@@ -92,13 +92,13 @@ class LSTMModel(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
-        self.lstm1 = nn.LSTM(input_size, hidden_size, num_layers=1, batch_first=True)
+        self.lstm1 = nn.LSTM(input_size, hidden_size, num_layers=self.num_layers, batch_first=True)
         self.bn1 = nn.BatchNorm1d(hidden_size)  
         
-        self.lstm2 = nn.LSTM(hidden_size, hidden_size, num_layers=1, batch_first=True)
+        self.lstm2 = nn.LSTM(hidden_size, hidden_size, num_layers=self.num_layers, batch_first=True)
         self.bn2 = nn.BatchNorm1d(hidden_size)  
         
-        self.lstm3 = nn.LSTM(hidden_size, hidden_size, num_layers=1, batch_first=True)
+        self.lstm3 = nn.LSTM(hidden_size, hidden_size, num_layers=self.num_layers, batch_first=True)
         self.bn3 = nn.BatchNorm1d(hidden_size)  
         
         self.dropout = nn.Dropout(DROPOUT)
@@ -106,8 +106,8 @@ class LSTMModel(nn.Module):
         self.fc = nn.Linear(hidden_size, output_size)
     
     def forward(self, x):
-        h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         
         out, _ = self.lstm1(x, (h0, c0))
         out = self._permute(out)
@@ -132,3 +132,98 @@ class LSTMModel(nn.Module):
     
     def _permute(self, out):
         return out.permute(0, 2, 1)
+
+
+class CNN_LSTM(nn.Module):
+    def __init__(self):
+        super(CNN_LSTM, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv1d(22, 64, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(64, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+
+            nn.Conv1d(64, 128, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+            nn.Dropout(DROPOUT),  
+
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Dropout(DROPOUT),  
+
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+            nn.Dropout(DROPOUT),  
+
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Dropout(DROPOUT),  
+
+            nn.Conv1d(128, 64, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(64, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Dropout(DROPOUT),  
+            
+            nn.Conv1d(64, 22, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(22, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ELU(),
+        )
+
+        self.lstm = nn.LSTM(22, 64, 3, batch_first=True, dropout=DROPOUT)
+
+        self.flattened_feature_nums = 22 * 120
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.flattened_feature_nums, 512),
+            nn.BatchNorm1d(512, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ReLU(inplace=True),
+            nn.Dropout(DROPOUT),
+            
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ReLU(inplace=True),
+            nn.Dropout(DROPOUT),
+
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128, eps=1e-06, momentum=0.2, affine=True),  
+            nn.ReLU(inplace=True),
+            nn.Dropout(DROPOUT),
+
+            nn.Linear(128, 4)  
+        )
+        
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def _initialize_weights(self):
+        for module in self.modules():
+            if isinstance(module, nn.Conv1d):
+                nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+            elif isinstance(module, nn.LSTM):
+                for name, param in module.named_parameters():
+                    if 'weight_ih' in name:
+                        nn.init.kaiming_normal_(param.data, mode='fan_in', nonlinearity='relu')
+                    elif 'weight_hh' in name:
+                        nn.init.kaiming_normal_(param.data, mode='fan_in', nonlinearity='relu')
+                    elif 'bias' in name:
+                        param.data.fill_(0)
+            elif isinstance(module, nn.Linear):
+                nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.constant_(module.bias, 0)
